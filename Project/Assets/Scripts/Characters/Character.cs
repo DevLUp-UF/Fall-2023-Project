9acae1ac002e6ae5220e277eae631fba4bebad41
Exanite <exanite78012@gmail.com>
2023-09-20 21:41:49 -0400

    Implement movement in the base Character class

--- Project/Assets/Scripts/Characters/Character.cs
+++ Project/Assets/Scripts/Characters/Character.cs
@@ -4,14 +4,49 @@ public abstract class Character : MonoBehaviour
 {
     [Header("Character Info")]
     [SerializeField]
-    protected int health = 1;
+    protected float health = 1;
     [SerializeField]
-    protected int maxHealth = 1;
+    protected float maxHealth = 1;
     [SerializeField]
-    protected int attack = 0;
+    protected float attack = 0;
+
+    [SerializeField]
+    protected float movementSpeed = 5;
 
     protected Rigidbody rb;
-    protected SpriteRenderer spriteRenderer;
 
+    protected virtual void Awake()
+    {
+        // GetComponent allows Character to interact with the Character's Rigidbody
+        // Character and Rigidbody *must* be on the same GameObject
+        rb = GetComponent<Rigidbody>();
+    }
+
+    protected void FixedUpdate()
+    {
+        // Read, modify, apply pattern
+        // Note: Modifying physics typically is done in FixedUpdate
+        
+        // --- Read ---
+        var movement = GetMovementInput();
+        
+        // --- Modify ---
+        // Multiply by movement speed.
+        movement *= movementSpeed;
+        
+        // Multiply by drag to counteract drag. This allows drag to control how responsive the character is.
+        // Lower drag makes the character feel more slippery, as if on ice.
+        movement *= rb.drag;
+        
+        // --- Apply ---
+        rb.AddForce(movement, ForceMode.VelocityChange);
+    }
+
+    // Allows subclasses of Character to control movement input
+    // For the player, this means the player can control the character using the keyboard
+    // For the enemies, this means the AI can control the character
+    protected abstract Vector2 GetMovementInput();
+
+    // Other classes can call this method to damage this character
     public abstract void TakeDamage(CharType type, int damage);
 }
