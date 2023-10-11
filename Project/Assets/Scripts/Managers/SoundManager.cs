af222fcda379742e68c7499f42063bd9fa8fd525
RobertKilkenny <robert.kilkenny7@gmail.com>
2023-10-10 23:16:54 -0400

    Misc Changes
    - Started sound work
    - Started developing Pause Menu UI
    - Added some prefabs

---
+++ Project/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : SingletonBehaviour<SoundManager>
+{
+    [SerializeField]
+    private AudioClip uiPauseOpen;
+
+    [SerializeField]
+    private List<AudioClip> menuMusicList;
+    [SerializeField]
+    private List<AudioClip> gameMusicList;
+
+    [SerializeField]
+    private AudioSource uiSource;
+    [SerializeField]
+    private AudioSource musicSource;
+    [SerializeField]
+    private AudioSource gameSource;
+    private bool isInGame = false;
+
+
+    public void InitSingleton()
+    {
+        isInGame = false;
+    }
+
+    // Make sure appropriate music is playing at all times
+    private void Update()
+    {
+        if (!musicSource.isPlaying)
+        {
+            if(isInGame)
+                PlayClip(gameMusicList[Random.Range(0, gameMusicList.Count)], musicSource);
+            else
+                PlayClip(menuMusicList[Random.Range(0, menuMusicList.Count)], musicSource);
+        }
+    }
+
+    public void FlipGameState()
+    {
+        isInGame = !isInGame;
+        if (isInGame)
+            PlayClip(gameMusicList[Random.Range(0, gameMusicList.Count)], musicSource);
+        else
+            PlayClip(menuMusicList[Random.Range(0, menuMusicList.Count)], musicSource);
+    }
+
+    public void UIPauseOpen() { PlayClip(uiPauseOpen, uiSource); }
+
+    // Plays the given clip in the given source
+    private void PlayClip(AudioClip clip, AudioSource src)
+    {
+        src.clip = clip;
+        src.Play();
+    }
+}
+
